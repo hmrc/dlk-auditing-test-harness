520bb2714ad42068fb217a1ebb70f47a7d695712
adrian.rapan <33421067+adrianrapan@users.noreply.github.com>
2021-08-10 14:47:35 +0100

    DLK-832 change scope of the project to allow for local testing of play-auditing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,11 +9,10 @@ object AppDependencies {
 
     "uk.gov.hmrc"             %% "govuk-template"           % "5.65.0-play-27" exclude ("uk.gov.hmrc", "play-auditing-play-27_2.12"),
     "uk.gov.hmrc"             %% "play-ui"                  % "9.1.0-play-27" exclude ("uk.gov.hmrc", "play-auditing-play-27_2.12"),
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "4.2.0" exclude ("uk.gov.hmrc", "play-auditing-play-27_2.12")
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "5.11.0-SNAPSHOT"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27"        % "4.2.0"              % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"               % Test,
     "org.scalacheck"          %% "scalacheck"               % "1.14.3"              % Test,
     "org.jsoup"               %  "jsoup"                    % "1.10.2"              % Test,
