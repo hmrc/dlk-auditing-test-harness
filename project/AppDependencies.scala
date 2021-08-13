f4c18a5e68fb48a5925a0c1ae37f1db7f9b74b9d
adrian.rapan <33421067+adrianrapan@users.noreply.github.com>
2021-08-13 10:47:01 +0100

    DLK-832 now all play-28,play-27 and play-26 use a snapshot of bootstrap-frontend-play which means they re all for local development. While the master branch will always be on bootstrap-frontend-play-28 with a published version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.11.0-SNAPSHOT"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.11.0"
   )
 }
