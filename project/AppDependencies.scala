79992bd3ad7d5d24901c1014b3da6963ff963eb6
Thomas Rynne <thomas.rynne@digital.hmrc.gov.uk>
2021-08-23 13:55:57 +0100

    DLK-896 Remove auth dependency
    auth need all services to upgrade their auth-client due to a breaking change
    
    This project doesn't use auth but it is safer to make that clear in the build and also means
    that this project will not show up in dependency explorer

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
-import sbt._
+import sbt.{ExclusionRule, _}
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.11.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.11.0" excludeAll (ExclusionRule("uk.gov.hmrc", "auth-client_2.12"))
   )
 }
